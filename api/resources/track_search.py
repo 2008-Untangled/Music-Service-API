35e0b61770876c447d5ac5017f97d78dadc96e23
Sean Steel <seanrsteel@gmail.com>
2021-03-22 10:52:44 -0600

    Update track_search method to return only the song data required instead of whole spotify response

--- api/resources/track_search.py
+++ api/resources/track_search.py
@@ -3,12 +3,28 @@ import requests
 from api.services.spotify_service import Spotify
 
 class Track:
-  def track_search(self):
+  def track_search(self, query):
     service = Spotify()
     token = service.access_token(service.authorization_string())
-    song_query = 'Yesterday'
+    song_query = query
     limit = 1
 
     response = service.track_search(token, song_query, limit)
     response_data = json.loads(response.text)
-    return response_data
+    response_data_song = response_data['tracks']['items'][0]
+    song_name = response_data['tracks']['items'][0]['name']
+    song_url = response_data['tracks']['items'][0]['external_urls']['spotify']
+    artist_name = response_data['tracks']['items'][0]['artists'][0]['name']
+    album_name = response_data['tracks']['items'][0]['album']['name']
+    album_release_date = response_data['tracks']['items'][0]['album']['release_date']
+
+    song_data = {
+      'data': {
+        'song': song_name,
+        'url': song_url,
+        'artist_name': artist_name,
+        'album_name': album_name,
+        'album_release_date': album_release_date
+      }
+    }
+    return song_data
