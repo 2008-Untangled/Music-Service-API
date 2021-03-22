f5987519b088f5f172b4892a35ace5cf38eef4db
Sean Steel <seanrsteel@gmail.com>
2021-03-22 11:54:07 -0600

    Add sad path test for spotify service when searching for random letters

--- tests/service/test_spotify_service.py
+++ tests/service/test_spotify_service.py
@@ -22,16 +22,14 @@ class SpotifyServiceTest(unittest.TestCase):
 
       response = service.track_search(token, song_query, limit)
       response_data = json.loads(response.text)
-
-      self.assertEqual(200, response.status_code)
-
-      response_data_song = response_data['tracks']['items'][0]
+      
       song_name = response_data['tracks']['items'][0]['name']
       song_url = response_data['tracks']['items'][0]['external_urls']['spotify']
       artist_name = response_data['tracks']['items'][0]['artists'][0]['name']
       album_name = response_data['tracks']['items'][0]['album']['name']
       album_release_date = response_data['tracks']['items'][0]['album']['release_date']
 
+      self.assertEqual(200, response.status_code)
       assert song_name == 'Yesterday - Remastered 2009'
       assert song_url == 'https://open.spotify.com/track/3BQHpFgAp4l80e1XslIjNI'
       assert artist_name == 'The Beatles'
@@ -52,3 +50,13 @@ class SpotifyServiceTest(unittest.TestCase):
       self.assertEqual(400, info['status'])
       self.assertEqual('No search query', info['message'])
 
+    def test_return_track_search(self):
+      service = Spotify()
+      token = service.access_token(service.authorization_string())
+      song_query = 'sdfhsdgh'
+      limit = 1
+
+      response = service.track_search(token, song_query, limit)
+      response_data = json.loads(response.text)
+      print(response_data)
+      self.assertEqual(0, response_data['tracks']['total'])
